2d5f6cb6a4de3e330de5629979296297a7f662b1
Sadia555 <sadiasehar555@yahoo.com>
2020-07-10 22:38:56 +0500

    Update App.js
    updated app name

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
     return (
       <div className={styles.container}>
        
-        <h1>Covid-19 Tracker </h1>
+        <h1>Covid-19 Tracker App </h1>
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} /> 
